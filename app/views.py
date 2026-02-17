c2816814f661a9c48c20b6e41e342f0daf5a9b1d
xosiyatrustamova <xosiyatrustamova0909@gmail.com>
2026-02-17 19:50:07 +0500

    template yozildi 4-kun

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, News
+
+
+# Create your views here.
+
+def news_list(request):
+    news_list = News.objects.filter(status=News.Status.Published)
+
+    context = {
+        'news_list': news_list
+    }
+
+    return render(request, 'news/news_list.html', context=context)
+
+def news_detail(request, id):
+    news = get_object_or_404(News, id=id, status=News.Status.Published)
+    context = {
+        'news': news
+    }
+    return render(request, 'news/news_detail.html', context=context)
+
+def home_page(request):
+    news_list = News.objects.filter(status=News.Status.Published)
+
+    context = {
+        'news_list': news_list
+    }
+
+    return render(request, 'news/index.html', context=context)
+
